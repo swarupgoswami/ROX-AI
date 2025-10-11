427ff361907a770308adef2cb603cc9b20ca8853
swarup Goswami <goswamiswarup97@gmail.com>
2025-10-11 09:24:33 +0530

    added the aiController

---
+++ backend/controllers/aiController.js
@@ -0,0 +1,71 @@
+const {GoogleGenAI}=require('@google/genai');
+const {conceptExplainPrompt, questionAnswerPrompt}=require('../utils/prompts');
+
+const ai=new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
+
+const generateInterviewQuestions=async(req,res)=>{
+    try{
+       const {role,experince,topicsToFocus,numberOfQuestions}=req.body;
+
+       if(!role || !experince || !topicsToFocus || !numberOfQuestions){
+        return res.status(400).json({
+            message:"Missing required fields"
+        });
+       }
+
+       const prompt=questionAnswerPrompt(role,experince,topicsToFocus,numberOfQuestions);
+
+       const response=await ai.models.generateContent({
+        model:"gemini-2.0-flash-lite",
+        contents:prompt,
+       })
+
+
+       let rawText=response.text;
+
+       const cleanedText=rawText.replace(/^```json\s*/,"").replace(/```$/,"").trim();
+
+       const data=JSON.parse(cleanedText);
+
+       res.status(200).json(data);
+    }catch(error){
+       res.status(500).json({message:"failed to genearte questions",
+        error:error.message
+       });
+
+    }
+};
+
+
+
+const generateConceptExplanation=async(req,res)=>{
+    try {
+        const {question}=req.body;
+        if(!question){
+            return res.status(400).json({message:"missing required fields"});
+        }
+
+
+        const prompt=conceptExplainPrompt(question);
+
+        const response= await ai.models.generateContent({
+            model:"gemini-2.0-flash-lite",
+            contents:prompt,
+        });
+
+
+        let rawText=response.text;
+
+        const cleanedText=rawText.replace(/^```json\s*/,"").replace(/```$/,"").trim();
+
+        const data=JSON.parse(cleanedText);
+
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({message:"failed to generate Concept explanation",error:error.message});
+    }
+};
+
+
+module.exports={generateInterviewQuestions,generateConceptExplanation};
