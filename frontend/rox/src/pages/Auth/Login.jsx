489194b44dd6499e5038dc80e9671c7fb7471381
swarup Goswami <goswamiswarup97@gmail.com>
2025-10-24 12:15:44 +0530

    integrated the login.jsx with backend

--- frontend/rox/src/pages/Auth/Login.jsx
+++ frontend/rox/src/pages/Auth/Login.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/Input"
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPath';
+import { UserContext } from '../../context/userContext';
 
 const Login = ({setCurrentPage}) => {
 
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const [error,setError]=useState(null);
+
+    const {updateUser}=useContext(UserContext);
 
     const navigate=useNavigate();
 
@@ -33,12 +38,23 @@ const Login = ({setCurrentPage}) => {
         // login api call 
 
         try {
-            
+            const response=await axiosInstance.post(API_PATHS.AUTH.LOGIN,{
+                email,
+                password,
+            });
+
+            const {token}=response.data;
+            if(token){
+                localStorage.setItem("token",token);
+                updateUser(response.data);
+                navigate('/dashboard');
+            }
         } catch (error) {
             if(error.response && error.data.message){
                 setError(error.response.data.message);
             }else{
                 setError("something went wrong please try again");
+                console.log(error);
             }
         }
     }
