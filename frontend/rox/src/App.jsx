68372fa07ff277f89aed5bd1303aac6278476eba
swarup Goswami <goswamiswarup97@gmail.com>
2025-10-25 15:56:33 +0530

    updated the app.jsx

--- frontend/rox/src/App.jsx
+++ frontend/rox/src/App.jsx
@@ -1,41 +1,45 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-
-import Login from './pages/Auth/Login';
-import SignUp from './pages/Auth/SignUp';
-import LandingPage from './pages/LandingPage';
-import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
-import Dashboard from './pages/Home/Dashboard';
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import LandingPage from "./pages/LandingPage";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
+import Dashboard from "./pages/Home/Dashboard";
+import UserProvider from "./context/userContext"
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage/>}/>
+    <UserProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
 
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
 
-          <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/dashboard" element={<Dashboard />} />
 
-          <Route path="/interview-prep:sessionId" element={<InterviewPrep/>}/>
-        </Routes>
-      </Router>    
+            <Route
+              path="/interview-prep/:sessionId"
+              element={<InterviewPrep />}
+            />
+          </Routes>
+        </Router>
 
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </UserProvider>
+  );
+};
 
-      <Toaster toastOptions={{
-        className:"",
-        style:{
-          fontSize:"13px",
-        },
-        }}
-      />
-    </div>
-  )
-}
-
-export default App
-
+export default App;
